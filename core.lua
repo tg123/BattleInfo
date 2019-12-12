f950ded319ea51c4c2f644a702c52cd2754956af
Boshi Lian <farmer1992@gmail.com>
2019-12-12 07:25:25 -0800

    hook battle update

--- core.lua
+++ core.lua
@@ -6,7 +6,7 @@ local battleList = {}
 local battleListOld = {}
 
 local function CloneToOld(map)
-    battleListOld[map] = {}
+    table.wipe(battleListOld[map])
 
     for instanceID in pairs(battleList[map] or {})  do
         battleListOld[map][instanceID] = true
@@ -14,27 +14,28 @@ local function CloneToOld(map)
 end
 
 local function UpdateBattleListCache()
-    -- print(1)
-    local b = GetBattlegroundInfo()
     local mapName = GetBattlegroundInfo()
 
+    if not mapName then
+        return
+    end
+
     CloneToOld(mapName)
-    battleList[mapName] = {}
+    table.wipe(battleList[mapName])
+    
     for i = 1, GetNumBattlefields()  do
         local instanceID = GetBattlefieldInstanceInfo(i)
         battleList[mapName][tonumber(instanceID)] = true
 	end
 end
 
-
-hooksecurefunc("JoinBattlefield", function()
-    UpdateBattleListCache()
-end)
+hooksecurefunc("JoinBattlefield", UpdateBattleListCache)
+hooksecurefunc("BattlefieldFrame_Update", UpdateBattleListCache)
 
 StaticPopupDialogs["CONFIRM_BATTLEFIELD_ENTRY"].OnShow = function(self)
     local tx = self.text:GetText()
 
-    if string.find(tx, L["OLD"], 1, 1) or string.find(tx, L["NEW"], 1 , 1) or string.find(tx, L["PERHAPS"], 1, 1) then			
+    if string.find(tx, L["Old"], 1, 1) or string.find(tx, L["New"], 1 , 1) or string.find(tx, L["Perhaps"], 1, 1) then			
         return
     end    
 
@@ -44,17 +45,17 @@ StaticPopupDialogs["CONFIRM_BATTLEFIELD_ENTRY"].OnShow = function(self)
         if toJ then
             if instanceIDs[toJ] then
                 local colorCode = RED_FONT_COLOR:GenerateHexColor()
-                local text = WrapTextInColorCode(L["OLD"], colorCode)
+                local text = WrapTextInColorCode(L["Old"], colorCode)
                 self.text:SetText(string.gsub(tx ,toJ , toJ .. "(" .. text .. ")"))
 
             elseif battleListOld[mapName][toJ] then
                 local colorCode = YELLOW_FONT_COLOR:GenerateHexColor()
-                local text = WrapTextInColorCode(L["PERHAPS"], colorCode)
+                local text = WrapTextInColorCode(L["Perhaps"], colorCode)
                 self.text:SetText(string.gsub(tx ,toJ , toJ .. "(" .. text .. ")"))
 
             else
                 local colorCode = GREEN_FONT_COLOR:GenerateHexColor()
-                local text = WrapTextInColorCode(L["NEW"], colorCode)
+                local text = WrapTextInColorCode(L["New"], colorCode)
                 self.text:SetText(string.gsub(tx ,toJ , toJ .. "(" .. text .. ")"))
 
             end
